ee702c146114d965ea9de2b9f957f87c3bb6bef9
Jess <jessachandler@gmail.com>
2019-03-11 16:42:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Slime App
+
+## Goals
+A fun visual app that uses motion control, swift, and no storyboards
+
+## Contribute
+PRs open
+
+## License
+GNU GPL 3.0
+;tldr - you can use this and distribute publicly (no selling)
---
+++ slimeapp/SlimeVC.swift
@@ -0,0 +1,20 @@
+//
+//  SlimeVC.swift
+//  slimeapp
+//
+//  Created by Jess Chandler on 3/11/19.
+//  GNU GPL v3.0
+//
+
+import UIKit
+
+class SlimeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
