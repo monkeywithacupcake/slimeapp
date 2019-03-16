1903966a4380aefe8ed9ee050fde629c9c488d36
Jess <jessachandler@gmail.com>
2019-03-16 05:39:20 -0700

    add a timer to continuously create new drops

--- slimeapp/SlimeVC.swift
+++ slimeapp/SlimeVC.swift
@@ -11,11 +11,8 @@ import UIKit
 class SlimeVC: UIViewController {
 
     // MARK: - Properties
-    //var oneDrop = DropView()
-    //var twoDrop = DropView()
+    var dropTimer: Timer!
     var drops = [DropView]()
-    var pinks = [PinkView]()
-    var mores = [DropView]()
     var animator: UIDynamicAnimator?
 
     // MARK: - Lifecycle
@@ -27,21 +24,25 @@ class SlimeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        dropTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(createAndAnimate), userInfo: nil, repeats: true)
+
         createDrops()
-        //createFallingObjects()
-        //animateObjects(a: pinks)
-        animateObjects(a: mores)
+        animateObjects(a: drops)
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        dropTimer.invalidate()
+    }
 
+    @objc func createAndAnimate(){
+        createDrops()
+        animateObjects(a: drops)
+    }
 
     func createDrops(){
-        //drops = [DropView(),DropView(),DropView(),DropView(),DropView()]
-        //pinks = [PinkView(),PinkView(),PinkView()]
-        for _ in 1...400 {
-            mores.append(DropView())
+        for _ in 1...10 {
+            drops.append(DropView())
         }
-        //mores = [DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView(),DropView()]
     }
     func animateObjects(a: [UIView]){
         a.forEach { ad in
@@ -74,36 +75,6 @@ class SlimeVC: UIViewController {
         animator?.addBehavior(bounce)
         animator?.addBehavior(gravity)
         animator?.addBehavior(pushes)
-    }
-
-    func createFallingObjects() {
-        drops.forEach { d in
-            d.removeFromSuperview()
-            self.view.addSubview(d)
-        }
-
-        //Initialize the animator
-        animator = UIDynamicAnimator(referenceView: self.view)
-
-        //Gravity
-        let gravity = UIGravityBehavior(items: drops)
-        let direction = CGVector(dx: 0.07, dy: 0.05)
-        gravity.gravityDirection = direction
-
-        //Collision
-        let boundaries = UICollisionBehavior(items: drops)
-        boundaries.translatesReferenceBoundsIntoBoundary = true
-
-        //Elasticity
-        let bounce = UIDynamicItemBehavior(items: drops)
-        bounce.elasticity = CGFloat.random(in: 0.0...1.0)
-        //func addLinearVelocity(_ velocity: CGPoint, for item: UIDynamicItem)
-
-
-
-        animator?.addBehavior(boundaries)
-        animator?.addBehavior(bounce)
-        animator?.addBehavior(gravity)
     }
 
 
